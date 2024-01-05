10ecfc7a7d8644da83b9a70ab541f8e65b546e8c
Nurdoka <nurdoolot93@gmail.com>
2024-01-06 05:56:10 +0600

    Configure JPA project

---
+++ JPAMessageList/src/main/java/com/skilldistillery/messagelist/entities/Message.java
@@ -0,0 +1,59 @@
+package com.skilldistillery.messagelist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+
+	public Message() {}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", content=" + content + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Message other = (Message) obj;
+		return id == other.id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
---
+++ JPAMessageList/src/test/java/com/skilldistillery/messagelist/entities/MessageTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.messagelist.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import net.bytebuddy.matcher.ElementMatcher;
+
+class MessageTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Message message;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMessageList");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		message = em.find(Message.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		message = null;
+	}
+
+	@Test
+	void test_Message_entity_mapping() {
+		assertNotNull(message);
+		assertEquals("Hello", message.getContent());
+	}
+
+}
