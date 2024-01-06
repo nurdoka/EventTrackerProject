61b5e8cf993d7a21c3200b13fe0b581777d1e7c8
Nurdoka <nurdoolot93@gmail.com>
2024-01-06 06:25:03 +0600

    Configure Spring REST project

---
+++ MessageList/src/main/java/com/skilldistillery/messagelist/MessageListApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.messagelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageListApplication.class, args);
+	}
+
+}
