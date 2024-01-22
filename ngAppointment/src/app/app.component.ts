aa2e87cce65596ffbbd49e26ae77f015b0907da6
Nurdoka <nurdoolot93@gmail.com>
2024-01-22 23:10:43 +0600

    create link between angular and AJAX

--- ngAppointment/src/app/app.component.ts
+++ ngAppointment/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,9 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     CommonModule,
     RouterOutlet,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    NotFoundComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- ngAppointment/src/app/app.routes.ts
+++ ngAppointment/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+
+];
